c36b5f8682a3810059c66564bfac0bc46d127784
liuxc <sciencestar9346@163.com>
2025-12-25 20:25:51 +0800

    update:增加机器人动作执行规划

--- src/demo_action_pkg/setup.py
+++ src/demo_action_pkg/setup.py
@@ -1,37 +1,30 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'demo_action_pkg'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
+    description='Demo Fibonacci Action Package',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = demo_action_pkg.publisher_node:main',
+            'listener = demo_action_pkg.subscriber_node:main',
+            'fibonacci_server = demo_action_pkg.fibonacci_server:main',
+            'fibonacci_client = demo_action_pkg.fibonacci_client:main',
+        ],
+    },
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-<<<<<<< HEAD
-        # 添加 action 文件
-        ('share/' + package_name + '/action', ['action/Fibonacci.action'])
-=======
->>>>>>> c575643 (update:项目更新)
+        ('share/' + package_name + '/action', ['action/Fibonacci.action']),
     ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='root',
-    maintainer_email='sciencestar9346@163.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
-    
-    entry_points={
-        'console_scripts': [
-        'talker = demo_action_pkg.publisher_node:main',
-        'listener = demo_action_pkg.subscriber_node:main',
-    ],
-    },
 )
